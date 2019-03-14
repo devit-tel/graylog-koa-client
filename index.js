580beb239229a400676766cad01072c4e24f74b6
Nantaphop Phuengphae <nantaphop.p@gmail.com>
2019-03-14 19:00:20 +0700

    remove duplicate

--- index.js
+++ index.js
@@ -46,36 +46,6 @@ const setConfig = ({
         }
     })    
 }
-gelfPro.setConfig({
-    fields: {
-        SERVICE: CONFIG.SERVICE,
-        NODE_ENV: CONFIG.NODE_ENV,
-    }, // optional; default fields for all messages
-    filter: [], // optional; filters to discard a message
-    transform: [], // optional; transformers for a message
-    broadcast: [], // optional; listeners of a message
-    levels: {}, // optional; default: see the levels section below
-    aliases: {}, // optional; default: see the aliases section below
-    adapterName: 'tcp', // optional; currently supported "udp", "tcp" and "tcp-tls"; default: udp
-    adapterOptions: { // this object is passed to the adapter.connect() method
-        // common
-        host: CONFIG.HOST, // optional; default: 127.0.0.1
-        port: CONFIG.PORT, // optional; default: 12201
-        // ... and so on
-
-        // tcp adapter example
-        family: 4, // tcp only; optional; version of IP stack; default: 4
-        timeout: 1000, // tcp only; optional; default: 10000 (10 sec)
-
-        // udp adapter example
-        protocol: 'udp4' // udp only; optional; udp adapter: udp4, udp6; default: udp4
-
-        // tcp-tls adapter example
-        //   key: fs.readFileSync('client-key.pem'), // tcp-tls only; optional; only if using the client certificate authentication
-        //   cert: fs.readFileSync('client-cert.pem'), // tcp-tls only; optional; only if using the client certificate authentication
-        //   ca: [fs.readFileSync('server-cert.pem')] // tcp-tls only; optional; only for the self-signed certificate
-    }
-})
 
 var levelsMapping = {
     emerg: 'emergency',
